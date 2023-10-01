384b75620b6949efacc89219776f27ae577161cf
toting <yoonzz123@gmail.com>
2023-10-01 16:48:10 +0900

    MVC update2

--- backend/config/index.js
+++ backend/config/index.js
@@ -7,14 +7,14 @@ const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
   logger.info("production mode");
-  dotenv.config({ path: path.join(__dirname, "../env/.env.production") });
+  dotenv.config({ path: path.join(__dirname, "../config/.env.production") });
 } else if (process.env.NODE_ENV === "development") {
   logger.info("development mode");
-  dotenv.config({ path: path.join(__dirname, "../env/.env.development") });
+  dotenv.config({ path: path.join(__dirname, "../config/.env.development") });
 } else {
   // 임시 로드용
   logger.info("---production mode---");
-  dotenv.config({ path: path.join(__dirname, "../env/.env.production") });
+  dotenv.config({ path: path.join(__dirname, "../config/.env.production") });
   // throw new Error("Couldn't find .env file");
 }
 
@@ -24,12 +24,13 @@ export default {
     prefix: "/", //라우터 접두사 ( localhost:5000/api로 기본설정 원할 시-> '/api' 설정하면됨)
   },
   //DB config
-  mysql_config: {
+  mysql: {
     HOST: process.env.MYSQL_HOST,
     USER: process.env.MYSQL_USER,
     PASSWORD: process.env.MYSQL_PASSWORD,
     dialect: "mysql",
     DB: process.env.MYSQL_DATABASE,
+    PORT: process.env.MYSQL_PORT,
     pool: {
       max: 5,
       min: 0,
